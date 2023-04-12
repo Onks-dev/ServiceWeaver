0d2fb4a85550a50ff00983a81044759d7e6eeb40
Onkabetse <onkabetse@luno.com>
2023-04-12 17:06:52 +0200

    Added a service weaver listener & http server

--- main/main.go
+++ main/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/ServiceWeaver/weaver"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -18,9 +19,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	reversed, err := reverser.Reverse(context, "Khumo")
+	lOpts := weaver.ListenerOptions{LocalAddress: "localhost:1234"}
+	l, err := root.Listener("reverseListener", lOpts)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(reversed)
+	fmt.Printf("reverseListener on %v\n", l)
+
+	http.HandleFunc("/reverse", func(writer http.ResponseWriter, request *http.Request) {
+		reversed, err := reverser.Reverse(context, request.URL.Query().Get("name"))
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(writer, "Reversed name, %s\n", reversed)
+	})
+	http.Serve(l, nil)
 }
